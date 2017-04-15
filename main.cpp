e1f5b5f61b28c3fc1dc4d4d044daed228be417c3
sobiemir <sobiemir@aculo.pl>
2017-04-15 15:38:09 +0200

    Add search statistics
    Search statistics are displaying when the search ends but before list of files, in which phrase was found. Search statistics includes information like how many files and folders was scanned, and in how many files the phrase was found.
    Issue #3

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/*
+ *  WordSearcher
+ *  Copyright (C) 2013-2017 sobiemir <sobiemir@aculo.pl>
+ * 
+ *  WordSearcher is free software: you can redistribute it and/or modify  
+ *  it under the terms of the GNU Lesser General Public License as   
+ *  published by the Free Software Foundation, version 3.
+ *
+ *  Word Searcher is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <string>
 #include <iostream>
@@ -5,9 +21,7 @@
 #include <vector>
 #include "dirent.h"
 
-
 using namespace std;
-
 
 int main( void )
 {
@@ -18,57 +32,51 @@ int main( void )
     string folder;
     string phrase;
 
+    int fileNums   = 0;
+    int folderNums = 0;
+
     vector<string> foundFolders;
     vector<string> phraseList;
 
     DIR *dirPointer;
     struct dirent *dirEntry;
 
-    // Informacje o programie
+    // informacje o programie
     cout << "===============================================================================" << endl;
-    cout << ": Program  : WordSearcher                                                     :" << endl;
-    cout << ": Autor    : Sobiemir                                                         :" << endl;
-    cout << ": Wersja   : 0.1                                                              :" << endl;
-    cout << ": Opis     : Program wyszukuje podana fraze w plikach (wyszukiwanie grupowe)  :" << endl;
+    cout << ": Program : WordSearcher                                                      :" << endl;
+    cout << ": Author  : sobiemir                                                          :" << endl;
+    cout << ": Version : 0.1.1                                                             :" << endl;
+    cout << ": Desc    : Searching for phrase in files located in given directory.         :" << endl;
     cout << "===============================================================================" << endl;
-    
-    // Informacje o programie nr. 2
-    cout << endl;
-    cout << "-------------------------------------------------------------------------------" << endl;
-    cout << ": Witaj w programie WordSearcher!                                             :" << endl;
-    cout << ": Zacial ci sie komputer? Podawaj to o co Cie program poprosi!                :" << endl;
-    cout << ": Podajac sciezke do folderu zawsze koncz nazwe slashem!                      :" << endl;
-    cout << "-------------------------------------------------------------------------------" << endl;
     
     while( continueSearch )
     {
-        // Ścieżka do folderu
+        // scieżka do folderu
         if( !saveLastFolder )
         {
             cout << endl << "Sciezka do folderu: ";
-            cin >> folder;
+            cin  >> folder;
         }
-        // Szukana fraza
+        // szukana fraza
         if( !saveLastWord )
         {
-            cout << endl;
-            cout << "Szukana fraza: ";
-            cin >> phrase;
+            cout << endl << "Szukana fraza: ";
+            cin  >> phrase;
         }
 
-        // Dodaj szukany folder do listy
+        // dodaj szukany folder do listy
         foundFolders.push_back( folder );
         cout << endl;
         cout << "Prosze czekac... trwa wyszukiwanie..." << endl;
         cout << "===============================================================================" << endl;
 
-        // Szukanie frazy
+        // szukanie frazy
         while( foundFolders.size( ) > 0 )
         {
             string currFolder   = foundFolders.at( 0 );
             dirPointer          = opendir( currFolder.c_str( ) );
 
-            // Wyświetlanie postępu - foldery
+            // wyświetlanie postępu - foldery
             cout << endl;
             cout << "Folder: " << currFolder << endl;
             cout << "-------------------------------------------------------------------------------" << endl;
@@ -77,55 +85,72 @@ int main( void )
             {
                 while( dirEntry = readdir( dirPointer ) )
                 {
-                    string  fileName = currFolder + dirEntry->d_name;
+                    string  fileName      = currFolder + dirEntry->d_name;
+                    string  shortFileName = currFolder + dirEntry->d_name;
                     string  textLine;
                     fstream fileStream;
 
-                    // Sprawdź czy znaleziony plik jest folderem
-                    if( dirEntry->d_type == DT_DIR && strcmp( dirEntry->d_name, "." ) && strcmp( dirEntry->d_name, ".." ) )
+                    // sprawdź czy znaleziony plik jest folderem
+                    if( dirEntry->d_type == DT_DIR )
                     {
-                        foundFolders.push_back( currFolder + dirEntry->d_name + "/" );
-                        continue;
+                        if( strcmp(dirEntry->d_name, ".") && strcmp(dirEntry->d_name, "..") )
+                            foundFolders.push_back( currFolder + dirEntry->d_name + "/" ),
+                            folderNums++;
                     }
-
-                    // Otwórz plik i wyświetl postęp - pliki
-                    fileStream.open( fileName, std::ios::in );
-                    cout << "   - " << fileName << endl;
-
-                    // Szukaj frazy
-                    while( getline( fileStream, textLine ) )
+                    else
                     {
-                        size_t foundPos = textLine.find( phrase );
-                        if( foundPos != std::string::npos )
+                        // otwórz plik i wyświetl postęp - pliki
+                        fileStream.open( fileName, std::ios::in );
+                        shortFileName.erase( 0, folder.size() );
+                        fileNums++;
+                        cout << "   - " << shortFileName << endl;
+
+                        // szukaj frazy
+                        while( getline( fileStream, textLine, '\n' ) )
                         {
-                            // Dodaj do listy znalezionych
-                            phraseList.push_back( fileName );
-                            break;
+                            size_t foundPos = textLine.find( phrase );
+                            if( foundPos != std::string::npos )
+                            {
+                                // dodaj do listy znalezionych
+                                phraseList.push_back( shortFileName );
+                                break;
+                            }
                         }
+                        fileStream.close();
                     }
-                    fileStream.close( );
                 }
             }
-            else cout << "Nie znaleziono folderu o takiej nazwie!";
+            else
+                cout << "Nie znaleziono folderu o takiej nazwie!";
 
-            // Zamnkij folder
+            // zamnkij folder
             closedir( dirPointer );
             foundFolders.erase( foundFolders.begin( ) );
         }
 
-        int phraseListSize  = phraseList.size( );
-        int userOption      = 0;
+        int phraseListSize = phraseList.size( );
+        int userOption     = 0;
 
-        // Wyświetlanie wyników
+        // wyświetlanie wyników
         if( phraseListSize > 0 )
         {
-            cout << endl << "Podana fraze znaleziono w nastepujacych plikach:" << endl;
+            cout << endl;
+            cout << "-------------------------------------------------------------------------------" << endl;
+            cout << "Przeszukiwany katalog: " << folder << endl;
+            cout << "Szukana fraza: " << phrase << endl;
+            cout << "Przeszukano " << fileNums  folderNums << " elementow, w tym:" << endl;
+            cout << "   - " << fileNums << " elementow to pliki." << endl;
+            cout << "   - " << folderNums << " elementow to foldery." << endl;
+            cout << "   - " << phraseListSize << " plikow spelnia kryteria wyszukiwania" << endl;
+            cout << "-------------------------------------------------------------------------------" << endl;
+            cout << "Lista plikow spelniajacych kryteria wyszukiwania:" << endl;
             cout << "-------------------------------------------------------------------------------" << endl;
             for( int x = 0; x < phraseListSize; ++x ) cout << "   - " << phraseList.at( x ) << endl;
         }
-        else cout << endl << "Podana fraza nie zostala znaleziona w zadnym pliku!" << endl;
+        else
+            cout << endl << "Podana fraza nie zostala znaleziona w zadnym pliku!" << endl;
 
-        // Menu dodatkowe
+        // menu dodatkowe
         cout << endl;
         cout << "[0] Wyjdz" << endl;
         cout << "[1] Nowe wyszukiwanie" << endl;
@@ -134,16 +159,16 @@ int main( void )
         cout << "[4] Szukaj innej frazy" << endl;
         cout << endl;
         cout << "Wybierz opcje: ";
-        cin >> userOption;
+        cin  >> userOption;
 
         // Od nowa...
         switch( userOption )
         {
-        case 1: saveLastFolder = false; saveLastWord = false; break;
-        case 2: saveLastFolder = true; saveLastWord = true; break;
-        case 3: saveLastFolder = false; saveLastWord = true; break;
-        case 4: saveLastFolder = true; saveLastWord = false; break;
-        default: continueSearch = false;
+            case 1:  saveLastFolder = false; saveLastWord = false; break;
+            case 2:  saveLastFolder = true;  saveLastWord = true;  break;
+            case 3:  saveLastFolder = false; saveLastWord = true;  break;
+            case 4:  saveLastFolder = true;  saveLastWord = false; break;
+            default: continueSearch = false;
         }
         // Czyszczenie listy wyników
         phraseList.clear( );
