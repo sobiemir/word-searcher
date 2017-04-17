66d24b983271cb6b3184c7ab077eeacc46333c18
sobiemir <sobiemir@aculo.pl>
2017-04-17 21:26:06 +0200

    Add filter for file extension
    Now, user can limit searching by typing filter for file extensions. User can type filter after typing phrase on the beginning.
    Template for filter: [modifiers]/[extension1]/[extension2]/...
    Details available in issue releated to this commit, or in readme.
    Also, fixed bug with string trimming - it was clearing string, when user type only one character.
    Issue #7

--- README.md
+++ README.md
@@ -1,18 +1,56 @@
 # WordSearcher
 
 Program pozwala na wyszukiwanie frazy w plikach, znajdujących się w podanym katalogu.
-Program działa w konsoli tak samo na systemie Windows, jak i na systemie Linux.
+Program działa w konsoli zarówno na systemie Windows, jak i na systemie Linux.
 
 W przypadku systemu Linux, jedyne co wystarczy zrobić to odpalić skrypt `compile.sh`.
 Po poprawnej kompilacji utworzony zostanie plik `wordsearcher`.
 
+### Filtrowanie
+
+Od wersji 0.1.4 możliwe jest filtrowanie rozszerzeń plików.
+Filtr można wpisać zaraz po podaniu frazy i powinien on mieć następujący format:
+
+    [modyfikatory]/[rozszerzenie1]/[rozszerzenie2]/...
+
+Gdzie modyfikatorami są:
+
+  * `!` : Porównywanie rozszerzeń z uwzględnieniem wielkości liter - szybsze niż zwykłe.
+  * `@` : Uwzględnienie w wyszukiwaniu plików, nie posiadających rozszerzeń.
+  * `*` : Wyszukiwanie we wszystkich plikach - domyślne gdy filtr jest pusty.
+  * `+` : Przeszukiwanie w plikach o podanych rozszerzeniach - parametr domyślny.
+  * `-` : Przeszukiwanie w plikach o rozszerzeniach innych niż podane.
+
+Modyfikatory można łączyć oraz podawać bez listy rozszerzeń.
+Przypadki podawania modyfikatorów bez listy rozszerzeń:
+
+  * `@` : Wyszukiwanie tylko w plikach bez rozszerzeń.
+  * `*@` : Wyszukiwanie w plikach posiadających rozszerzenia.
+  * `-` : Wolniejszy odpowiednik pozycji `*@`.
+  
+Przykłady poprawnych filtrów:
+
+    *@
+    !+/html/css
+    -/jpg/png
+    /php/js/ts
+    !@-/cpp/hpp
+
+### Wygląd
+
 Po uruchomieniu program będzie wyglądał mniej więcej tak:
 
-![WordSearcher v0.1.3](http://img.aculo.pl/word-searcher/v0.1.3-start.png)
+![WordSearcher v0.1.4](http://img.aculo.pl/word-searcher/v0.1.4-start.png)
 
-Pierwszą rzeczą o którą program poprosi jest folder w którym program będzie szukał frazy w plikach.
-Kolejną rzeczą jest fraza.
-Po podaniu frazy i wciśnięciu przycisku ENTER, aplikacja rozpocznie wyszukiwanie.
+Program na początku przejdzie w tryb interakcji z użytkownikiem, prosząc o podanie kolejno:
+
+  * Folderu w którym program będzie szukał plików.
+  * Frazy, która będzie wyszukiwana w plikach.
+  * Filtru, stosowanego na rozszerzeniach wszystkich znalezionych plików.
+
+Po filtru i wciśnięciu przycisku ENTER, aplikacja rozpocznie wyszukiwanie.
 Po zakończonym wyszukiwaniu pojawią się statystyki i menu wyboru kolejnej akcji.
 
-![WordSearcher v0.1.3](http://img.aculo.pl/word-searcher/v0.1.3-menu.png)
+![WordSearcher v0.1.4](http://img.aculo.pl/word-searcher/v0.1.4-menu.png)
+
+Program po podaniu wartości spoza zakresu (innej niż wypisane w menu) powtórzy pytanie i będzie oczekiwał na porawną wartość.
--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,11 @@
 #   define MSD_SYSTEM_LINUX
 #endif
 
+#define WSD_MOD_ALL           0x1
+#define WSD_MOD_CASESENSITIVE 0x2
+#define WSD_MOD_EXCLUDE       0x4
+#define WSD_MOD_NOEXT         0x8
+
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -45,7 +50,7 @@ using namespace std;
  */
 int trim_charstr( char *data, size_t len )
 {
-    int pos = 0;
+    size_t pos = 0;
 
     if( len == 0 )
         len = strlen( data );
@@ -69,10 +74,154 @@ int trim_charstr( char *data, size_t len )
             break;
         }
 
-    if( pos == 0 )
+    if( pos == 0 && (data[0] == ' ' || data[0] == '\0') )
         data[0] = '\0';
+    else if( pos == 0 )
+        data[++pos] = '\0';
 
     return pos;
+}
+
+/**
+ * Parsuje rozszerzenia zamieniając je na listę.
+ * !+/html/css
+ * Taki ciąg po parsowaniu tworzy dwa rozszerzenia: html, css
+ * Dodatkowo dwa modyfikatory.
+ *
+ * Lista modyfikatorów:
+ * > ! : sprawdzanie rozszerzeń zgodnie z wielkością liter - szybsze przetwarzanie
+ * > + : modyfikator opcjonalny, tylko pliki z podanymi rozszerzeniami są przeszukiwane
+ * > - : tylko pliki o rozszerzeniach innych niż podane są przesukiwane
+ * > * : przeszukiwane są wszystkie rozszerzenia
+ * > @ : uwzględnij w filtrze również pliki bez rozszerzeń
+ *
+ * Szczególne przypadki podawania modyfikatorów bez rozszerzeń
+ * > @  : bez podawania rozszerzeń - szuka tylko w plikach bez rozszerzeń
+ * > *@ : szuka tylko w plikach z rozszerzeniami
+ * > -  : bez podawania rozszerzeń - szuka tylko w plikach z rozszerzeniami, wolniejsza wersja *@
+ *
+ * @param  str    Ciąg znaków zawierający rozszerzenia.
+ * @param  extvec Lista rozszerzeń wyciągniętych z ciągu.
+ * @return        Modyfikatory zamienione na liczbę.
+ */
+int parse_extensions( char *str, vector<string> &extvec )
+{
+    int modifier  = 0;
+    string extstr = str;
+
+    // wyczyść poprzednie filtry
+    extvec.clear();
+
+    size_t occ = extstr.find( '/', 0 );
+    while( occ != string::npos )
+    {
+        int start = occ;
+        occ = extstr.find( '/', occ + 1 );
+
+        string data;
+        if( occ != string::npos )
+            data = extstr.substr( start + 1, occ - (start + 1) );
+        else
+            data = extstr.substr( start + 1 );
+
+        if( !data.empty() )
+            extvec.push_back( data );
+    }
+
+    for( int x = 0, y = extstr.size(); x > -1 && x < y; ++x )
+        switch( extstr[x] )
+        {
+            case '/': x = -2; break;
+            case '*': modifier |= WSD_MOD_ALL; break;
+            case '-': modifier |= WSD_MOD_EXCLUDE; break;
+            case '!': modifier |= WSD_MOD_CASESENSITIVE; break;
+            case '@': modifier |= WSD_MOD_NOEXT; break;
+        }
+
+    // zamień na małe litery w przypadku gdy wyszukiwanie nie uwzględnia wielkości znaków
+    if( !(modifier & WSD_MOD_CASESENSITIVE) )
+        for( size_t x = 0; x < extvec.size(); ++x )
+            for( size_t y = 0; y < extvec[x].size(); ++y )
+                extvec[x][y] = tolower( extvec[x][y] );
+
+    return modifier;
+}
+
+/**
+ * Sprawdza, czy rozszerzenie pasuje do zdefiniowanych kryteriów.
+ *
+ * @param  filename Nazwa pliku.
+ * @param  extvec   Lista rozszerzeń wyciągniętych z ciągu.
+ * @param  modifier Modyfikatory.
+ * @return          Modyfikatory zamienione na liczbę.
+ */
+bool file_extension_check( string filename, vector<string> &extvec, int modifier )
+{
+    int dotpos = -1;
+
+    if( (modifier & (WSD_MOD_NOEXT | WSD_MOD_ALL)) == WSD_MOD_ALL )
+        return true;
+
+    // wyszukaj kropkę w nazwie pliku
+    for( int x = filename.size() - 1; x >= 0; --x )
+    {
+        if( filename[x] == '/' )
+            break;
+        else if( filename[x] == '.' )
+        {
+            dotpos = x + 1;
+            break;
+        }
+    }
+
+    // plik bez rozszerzenia domyślnie są odrzucane, po przekazaniu modyfikatora już nie
+    if( dotpos == -1 )
+    {
+        if( (modifier & (WSD_MOD_NOEXT | WSD_MOD_ALL)) == WSD_MOD_NOEXT )
+            return true;
+        else
+            return false;
+    }
+    else if( modifier & WSD_MOD_ALL )
+        return true;
+
+    const char *ext = &filename.c_str()[dotpos];
+    bool        cmp = false;
+    size_t      len = filename.size() - dotpos;
+
+    // sprawdź czy rozszerzenie się zgadza, zgodnie z wybraną techniką
+    // albo uwzględnia wielkość liter, albo nie
+    if( modifier & WSD_MOD_CASESENSITIVE )
+    {
+        for( size_t x = 0; x < extvec.size(); ++x )
+            if( len == extvec[x].size() && strcmp(ext, extvec[x].c_str()) == 0 )
+            {
+                cmp = true;
+                break;
+            }
+    }
+    else
+    {
+        // w tym przypadku trzeba sprawdzać każdy znak po kolei
+        for( size_t x = 0; x < extvec.size(); ++x )
+            if( len == extvec[x].size() )
+            {
+                cmp = true;
+                for( size_t y = 0; y < len; ++y )
+                    if( extvec[x][y] != tolower(ext[y]) )
+                    {
+                        cmp = false;
+                        break;
+                    }
+                if( !cmp )
+                    break;
+            }
+    }
+
+    // zaneguj, gdy pliki z podanym rozszerzeniem są pomijane
+    return modifier & WSD_MOD_EXCLUDE
+        ? !cmp
+        : cmp;
 }
 
 /**
@@ -84,17 +233,22 @@ int main( void )
     bool continueSearch = true;
     bool saveLastFolder = false;
     bool saveLastWord   = false;
+    bool saveLastFilter = false;
     
     char phrase[2048];
     char folder[2048];
+    char filter[2048];
 
     size_t folderLen;
+    size_t filterLen;
 
     vector<string> foundFolders;
     vector<string> phraseList;
+    vector<string> extList;
     
-    int fileNums = 0;
-    int folderNums = 0;
+    int fileNums     = 0;
+    int folderNums   = 0;
+    int extModifiers = 0;
 
     DIR *dirPointer;
     struct dirent *dirEntry;
@@ -103,16 +257,17 @@ int main( void )
     cout << "===============================================================================" << endl;
     cout << ": Program : WordSearcher                                                      :" << endl;
     cout << ": Author  : sobiemir                                                          :" << endl;
-    cout << ": Version : 0.1.3                                                             :" << endl;
+    cout << ": Version : 0.1.4                                                             :" << endl;
     cout << ": Desc    : Searching for phrase in files located in given directory.         :" << endl;
     cout << "===============================================================================" << endl;
+    cout << endl;
 
     while( continueSearch )
     {
         // ścieżka do folderu
         if( !saveLastFolder )
         {
-            cout << endl << "Folder [./]: ";
+            cout << "Folder [./]: ";
             cin.getline( folder, sizeof(folder) );
 
             folderLen = trim_charstr( folder, 0 );
@@ -133,15 +288,28 @@ int main( void )
         // szukana fraza
         if( !saveLastWord )
         {
-            cout << endl << "Fraza: ";
+            cout << "Fraza: ";
             cin.getline( phrase, sizeof(phrase) );
+        }
+        // filtr dla plików
+        if( !saveLastFilter )
+        {
+            cout << "Filtr [*]: ";
+            cin.getline( filter, sizeof(filter) );
+
+            filterLen = trim_charstr( filter, 0 );
+            if( filterLen == 0 )
+                filter[0] = '*',
+                filter[1] = '\0';
+
+            extModifiers = parse_extensions( filter, extList );
         }
 
         // dodaj szukany folder do listy
         foundFolders.push_back( folder );
         cout << endl;
         cout << "Wyszukiwanie..." << endl;
-        cout << "===============================================================================" << endl;
+        cout << "-------------------------------------------------------------------------------" << endl;
 
         // szukanie frazy
         while( foundFolders.size() > 0 )
@@ -152,7 +320,7 @@ int main( void )
             // wyświetlanie postępu - foldery
             cout << endl;
             cout << "Folder: " << currFolder << endl;
-            cout << "-------------------------------------------------------------------------------" << endl;
+            cout << "===============================================================================" << endl;
 
             if( dirPointer != NULL )
             {
@@ -172,8 +340,12 @@ int main( void )
                     }
                     else
                     {
+                        // pomijanie plików, które nie spełniają kryteriów
+                        if( !file_extension_check(fileName, extList, extModifiers) )
+                            continue;
+
                         // otwórz plik i wyświetl postęp - pliki
-                        fileStream.open( fileName, std::ios::in );
+                        fileStream.open( fileName.c_str(), std::ios::in );
                         shortFileName.erase( 0, folderLen );
                         fileNums++;
                         cout << "   - " << shortFileName << "..." << endl;
@@ -201,21 +373,23 @@ int main( void )
             foundFolders.erase( foundFolders.begin() );
         }
 
-        int phraseListSize = phraseList.size();
-        int userOption     = 0;
+        int  phraseListSize = phraseList.size();
+        char userOption     = -1;
+
+        cout << endl;
+        cout << "-------------------------------------------------------------------------------" << endl;
+        cout << "Przeszukiwany katalog: " << folder << endl;
+        cout << "Szukana fraza: " << phrase << endl;
+        cout << "Uzyty filtr: " << filter << endl;
+        cout << "Przeszukano " << fileNums + folderNums << " elementow, w tym:" << endl;
+        cout << "   - " << fileNums << " elementow to pliki." << endl;
+        cout << "   - " << folderNums << " elementow to foldery." << endl;
+        cout << "   - " << phraseListSize << " plikow spelnia kryteria wyszukiwania" << endl;
+        cout << "-------------------------------------------------------------------------------" << endl;
 
         // wyświetlanie wyników
         if( phraseListSize > 0 )
         {
-            cout << endl;
-            cout << "-------------------------------------------------------------------------------" << endl;
-            cout << "Przeszukiwany katalog: " << folder << endl;
-            cout << "Szukana fraza: " << phrase << endl;
-            cout << "Przeszukano " << fileNums + folderNums << " elementow, w tym:" << endl;
-            cout << "   - " << fileNums << " elementow to pliki." << endl;
-            cout << "   - " << folderNums << " elementow to foldery." << endl;
-            cout << "   - " << phraseListSize << " plikow spelnia kryteria wyszukiwania" << endl;
-            cout << "-------------------------------------------------------------------------------" << endl;
             cout << "Lista plikow spelniajacych kryteria wyszukiwania:" << endl;
             cout << "-------------------------------------------------------------------------------" << endl;
 
@@ -223,44 +397,56 @@ int main( void )
                 cout << "   - " << phraseList.at( x ) << endl;
         }
         else
-        {
-            cout << endl;
-            cout << "-------------------------------------------------------------------------------" << endl;
-            cout << "Przeszukiwany katalog: " << folder << endl;
-            cout << "Szukana fraza: " << phrase << endl;
-            cout << "Przeszukano " << fileNums + folderNums << " elementow, w tym:" << endl;
-            cout << "   - " << fileNums << " elementow to pliki." << endl;
-            cout << "   - " << folderNums << " elementow to foldery." << endl;
-            cout << "   - " << phraseListSize << " plikow spelnia kryteria wyszukiwania" << endl;
-            cout << "-------------------------------------------------------------------------------" << endl;
             cout << "Podana fraza nie zostala znaleziona w zadnym pliku!" << endl;
-        }
 
-        // menu dodatkowe
-        cout << endl;
-        cout << "[0] Wyjdz" << endl;
-        cout << "[1] Nowe wyszukiwanie" << endl;
-        cout << "[2] Szukaj ponownie" << endl;
-        cout << "[3] Szukaj w innym folderze" << endl;
-        cout << "[4] Szukaj innej frazy" << endl;
-        cout << endl;
-        cout << "Wybierz opcje: ";
-        cin  >> userOption;
+        cout << "-------------------------------------------------------------------------------" << endl;
 
-        // od nowa...
-        switch( userOption )
+        bool chooseMenu = true;
+        while( chooseMenu )
         {
-            case 1:  saveLastFolder = false; saveLastWord = false; break;
-            case 2:  saveLastFolder = true;  saveLastWord = true;  break;
-            case 3:  saveLastFolder = false; saveLastWord = true;  break;
-            case 4:  saveLastFolder = true;  saveLastWord = false; break;
-            default: continueSearch = false;
+            // menu dodatkowe
+            cout << endl;
+            cout << "[1] Nowe wyszukiwanie" << endl;
+            cout << "[2] Szukaj ponownie" << endl;
+            cout << "[3] Szukaj w innym folderze" << endl;
+            cout << "[4] Szukaj innej frazy" << endl;
+            cout << "[5] Szukaj z uzyciem innego filtra" << endl;
+            cout << "---" << endl;
+            cout << "[6] Szukaj innej frazy z innym filtrem" << endl;
+            cout << "[7] Szukaj w innym folderze z uzyciem innego filtra" << endl;
+            cout << "[8] Szukaj innej frazy w innym folderze" << endl;
+            cout << "---" << endl;
+            cout << "[0] Wyjdz" << endl;
+            cout << endl;
+            cout << "Wybierz opcje: ";
+            cin  >> userOption;
+
+            // od nowa...
+            chooseMenu = false;
+            switch( userOption )
+            {
+                case '0':  continueSearch = false; break;
+                case '1':  saveLastFolder = false; saveLastWord = false; saveLastFilter = false; break;
+                case '2':  saveLastFolder = true;  saveLastWord = true;  saveLastFilter = true;  break;
+                case '3':  saveLastFolder = false; saveLastWord = true;  saveLastFilter = true;  break;
+                case '4':  saveLastFolder = true;  saveLastWord = false; saveLastFilter = true;  break;
+                case '5':  saveLastFolder = true;  saveLastWord = true;  saveLastFilter = false; break;
+                case '6':  saveLastFolder = true;  saveLastWord = false; saveLastFilter = false; break;
+                case '7':  saveLastFolder = false; saveLastWord = true;  saveLastFilter = false; break;
+                case '8':  saveLastFolder = false; saveLastWord = false; saveLastFilter = true;  break;
+                default:
+                    chooseMenu = true;
+                    cin.ignore( INT_MAX, '\n' );
+                    cin.clear();
+            }
         }
         // czyszczenie listy wyników
         phraseList.clear();
 
         cin.ignore( INT_MAX, '\n' );
         cin.clear();
+
+        cout << endl;
     }
     return 0;
 }
