d915335613cd802211809d83b4af89793d3b44aa
sobiemir <sobiemir@aculo.pl>
2017-04-18 11:49:29 +0200

    Insert license notices

--- src/interface.hpp
+++ src/interface.hpp
@@ -1,3 +1,20 @@
+/*
+ *  WordSearcher
+ *  Copyright (C) 2013-2017 sobiemir <sobiemir@aculo.pl>
+ * 
+ *  This program is free software: you can redistribute it and/or modify  
+ *  it under the terms of the GNU Lesser General Public License as   
+ *  published by the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __WSH_INTERFACE__
 #define __WSH_INTERFACE__
 
